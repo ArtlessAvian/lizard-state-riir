52fbfcbd63c2043270d93916be025d0db4b5d152
Ryan <artlessavian@gmail.com>
2024-03-10 00:13:49 -0800

    more glue

--- godot-glue/src/lib.rs
+++ godot-glue/src/lib.rs
@@ -3,7 +3,7 @@ use std::rc::Rc;
 use engine::actions::GoRightAction;
 use engine::data::ActionTrait;
 use engine::data::CommandTrait;
-use engine::data::Entity;
+use engine::data::Entity as EntityInternal;
 use engine::data::Floor as FloorInternal;
 use godot::prelude::*;
 
@@ -30,7 +30,7 @@ impl IRefCounted for Floor {
 impl Floor {
     #[func]
     fn add_entity(&mut self) {
-        self.floor = self.floor.add_entity(Rc::new(Entity { x: 3 }));
+        self.floor = self.floor.add_entity(Rc::new(EntityInternal { x: 3 }));
     }
 
     #[func]
@@ -39,5 +39,101 @@ impl Floor {
             .verify_action(&self.floor, &self.floor.get_player())
             .unwrap()
             .do_action(&self.floor);
+    }
+
+    #[func]
+    fn get_player(&self) -> Gd<Entity> {
+        Entity::new(self.floor.get_player())
+    }
+
+    #[func]
+    fn get_action(&self) -> Gd<Action> {
+        Action::new(Box::new(GoRightAction))
+    }
+}
+
+#[derive(GodotClass)]
+struct Entity {
+    entity: Option<Rc<EntityInternal>>,
+}
+
+#[godot_api]
+impl IRefCounted for Entity {
+    fn init(_base: Base<RefCounted>) -> Self {
+        Self { entity: None }
+    }
+}
+
+#[godot_api]
+impl Entity {
+    fn new(entity: Rc<EntityInternal>) -> Gd<Self> {
+        Gd::from_init_fn(|_base| Self {
+            entity: Some(entity),
+        })
+    }
+}
+
+#[derive(GodotClass)]
+struct Action {
+    // Godot doesn't see this anyways.
+    action: Box<dyn ActionTrait>,
+}
+
+#[godot_api]
+impl IRefCounted for Action {
+    // Return a null action.
+    fn init(_base: Base<RefCounted>) -> Self {
+        Self {
+            action: Box::new(GoRightAction),
+        }
+    }
+}
+
+#[godot_api]
+impl Action {
+    fn new(action: Box<dyn ActionTrait>) -> Gd<Self> {
+        Gd::from_init_fn(|_base| Self { action })
+    }
+
+    #[func]
+    fn to_command(&self, floor: Gd<Floor>, subject: Gd<Entity>) -> Option<Gd<Command>> {
+        let binding = subject.bind();
+        let subject_ref = binding.entity.as_ref()?;
+        let verify_action = self
+            .action
+            .verify_action(&floor.bind().floor, subject_ref)?;
+        Some(Command::new(verify_action))
+    }
+}
+
+#[derive(GodotClass)]
+struct Command {
+    // Godot doesn't see this anyways.
+    command: Option<Box<dyn CommandTrait>>,
+}
+
+#[godot_api]
+impl IRefCounted for Command {
+    // Return an invalid command. Don't call this.
+    fn init(_base: Base<RefCounted>) -> Self {
+        Self { command: None }
+    }
+}
+
+#[godot_api]
+impl Command {
+    fn new(command: Box<dyn CommandTrait>) -> Gd<Self> {
+        Gd::from_init_fn(|_base| Self {
+            command: Some(command),
+        })
+    }
+
+    #[func]
+    fn do_action(&self, mut floor_mut: Gd<Floor>) {
+        let bind = Gd::bind(&floor_mut);
+        let next = self.command.as_ref().unwrap().do_action(&bind.floor);
+        drop(bind);
+        let mut bind_mut = Gd::bind_mut(&mut floor_mut);
+        bind_mut.floor = next;
     }
 }
--- godot/test.gd
+++ godot/test.gd
@@ -1,17 +1,20 @@
 extends Node2D
 
+var floor: Floor;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var yay = Floor.new();
-	yay.add_entity();
-	yay.go_right();
-	yay.go_right();
-	yay.go_right();
-	yay.go_right();
-	yay.go_right();
-
+	floor = Floor.new();
+	floor.add_entity();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var i = 0
 func _process(delta):
-	pass
+	print(i)
+	i += 1
+	
+	var player = floor.get_player()
+	var action = floor.get_action();
+	
+	var command = action.to_command(floor, player)
+	command.do_action(floor)
