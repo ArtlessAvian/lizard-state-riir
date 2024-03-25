3168f067ba88ab4fae440db8e3fcc70e09c3b24b
Ryan <artlessavian@gmail.com>
2024-03-24 23:46:15 -0700

    Schmovement

--- godot/test.gd
+++ godot/test.gd
@@ -2,23 +2,36 @@ extends Node
 
 var floor: Floor;
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	floor = Floor.new();
 	floor.add_entity();
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-var i = 0
 func _process(delta):
-	i += 1
-	if i % 100 != 0:
-		return
+	poll_input()
 	
-	var dir = Vector2i(randi_range(-1, 1), randi_range(-1, 1))
-	var action = floor.get_step_action(dir)
 	var player = floor.get_player()
-	print(player.get_pos())
 	$WorldRotate/InternalRotation/Entity.position = Vector3(player.get_pos().x, 0, player.get_pos().y)
-	
+
+func poll_input():
+	if Input.is_action_just_pressed("move_left"):
+		move_player(Vector2i.LEFT)
+	if Input.is_action_just_pressed("move_up"):
+		move_player(Vector2i.UP)
+	if Input.is_action_just_pressed("move_down"):
+		move_player(Vector2i.DOWN)
+	if Input.is_action_just_pressed("move_right"):
+		move_player(Vector2i.RIGHT)
+	if Input.is_action_just_pressed("move_upleft"):
+		move_player(Vector2i.UP + Vector2i.LEFT)
+	if Input.is_action_just_pressed("move_upright"):
+		move_player(Vector2i.UP + Vector2i.RIGHT)
+	if Input.is_action_just_pressed("move_downleft"):
+		move_player(Vector2i.DOWN + Vector2i.LEFT)
+	if Input.is_action_just_pressed("move_downright"):
+		move_player(Vector2i.DOWN + Vector2i.RIGHT)
+
+func move_player(dir: Vector2i):
+	var player = floor.get_player()
+	var action : Action = floor.get_step_action(dir)
 	var command = action.to_command(floor, player)
 	floor = command.do_action(floor)
