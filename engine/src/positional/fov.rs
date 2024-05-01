c712b195e8afd689ec487fb8f8d5741e8b385652
Ryan <artlessavian@gmail.com>
2024-05-01 03:06:25 -0700

    Calculate FOV by Traversing Data Structure.

--- engine/src/positional/fov.rs
+++ engine/src/positional/fov.rs
@@ -1,4 +1,7 @@
-use super::{algorithms::Segment, OctantRelative};
+use super::algorithms::Segment;
+use super::AbsolutePosition;
+use super::OctantRelative;
+use super::RelativePosition;
 
 type TrieEdge = Option<Box<TrieNode>>;
 
@@ -74,7 +77,7 @@ impl StrictFOV {
             ),
         };
 
-        for _ in 1..radius {
+        for _ in 1..=radius {
             partial.increase_radius()
         }
 
@@ -95,10 +98,116 @@ impl StrictFOV {
             }
         }
     }
+
+    /// Returns a Vec of all tiles visible. A tile that blocks vision is itself visible, but there's no vision past it.
+    /// There is no attempt to dedup tiles!
+    pub fn get_field_of_view_tiles<F: Fn(AbsolutePosition) -> bool>(
+        &self,
+        center: AbsolutePosition,
+        blocks_vision: F,
+    ) -> Vec<AbsolutePosition> {
+        self.get_field_of_view_tiles_relative(|x| blocks_vision(center + x))
+            .into_iter()
+            .map(|x| center + x)
+            .collect()
+    }
+
+    fn get_field_of_view_tiles_relative<F: Fn(RelativePosition) -> bool>(
+        &self,
+        blocks_vision_relative: F,
+    ) -> Vec<RelativePosition> {
+        let mut out = Vec::new();
+        for octant in 0..8 {
+            // DFS through the trie. BFS would return tiles in order of radius, which is nice.
+            let mut frontier = vec![&self.origin];
+            while let Some(current) = frontier.pop() {
+                let mut tile = current.tile;
+                tile.octant = octant;
+                let relative = RelativePosition::from(tile);
+
+                // This is what makes StrictFov "strict".
+                // This is equivalent to drawing a segment to the tile and checking everything in between.
+                // Without this, you are allowed to draw a segment *through* the tile and see it.
+                if current.generator == current.tile {
+                    out.push(relative);
+                }
+
+                if !blocks_vision_relative(relative) {
+                    if let Some(diag) = &current.diag {
+                        frontier.push(diag)
+                    }
+                    if let Some(run) = &current.run {
+                        frontier.push(run)
+                    }
+                }
+            }
+        }
+        out
+    }
 }
 
 #[cfg(test)]
 #[test]
 fn new_strict_fov() {
     StrictFOV::new(5);
+}
+
+#[cfg(test)]
+#[test]
+fn get_fov() {
+    let fov = StrictFOV::new(2);
+    let blocks_vision = |_| false;
+    let tiles = fov.get_field_of_view_tiles(AbsolutePosition::new(0, 0), blocks_vision);
+
+    for x in -2..=2 {
+        for y in -2..=2 {
+            assert!(
+                tiles.contains(&AbsolutePosition::new(x, y)),
+                "{} {} {:?}",
+                x,
+                y,
+                tiles
+            )
+        }
+    }
+
+    let mut tiles = tiles;
+    tiles.sort_by_key(|pos| (pos.x, pos.y));
+    tiles.dedup();
+    assert_eq!(tiles.len(), 5 * 5)
+    // Because we checked 25 unique items, and the vec has length 25, we have set equality.
+}
+
+#[cfg(test)]
+#[test]
+fn get_fov_with_obstruction() {
+    let fov = StrictFOV::new(2);
+    let blocks_vision = |pos: AbsolutePosition| pos.x > 0 && -1 <= pos.y && pos.y <= 1;
+    let tiles = fov.get_field_of_view_tiles(AbsolutePosition::new(0, 0), blocks_vision);
+
+    // @#
+    // .#
+    // ..
+
+    for y in -2..=2 {
+        assert!(tiles.contains(&AbsolutePosition::new(0, y))); // open space
+        assert!(tiles.contains(&AbsolutePosition::new(1, y))); // visible wall. (or nothing.)
+        assert!(!tiles.contains(&AbsolutePosition::new(2, y))); // space blocked by wall.
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn get_fov_with_direct_only() {
+    let fov = StrictFOV::new(4);
+    let blocks_vision = |pos: AbsolutePosition| pos.x / 3 != pos.y;
+    let tiles = fov.get_field_of_view_tiles(AbsolutePosition::new(0, 0), blocks_vision);
+
+    // We can see (4, 1) passing through (3, 1), but we can't see (3, 1) itself.
+    // This causes a discontinuity, which is ugly but whatever. This makes sense for "game logic" like hitting each other.
+    // @..##
+    // ### .
+
+    assert!(tiles.contains(&AbsolutePosition::new(4, 1)));
+    assert!(!tiles.contains(&AbsolutePosition::new(3, 1)));
 }
