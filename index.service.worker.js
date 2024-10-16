8bddae09d4ab534d1b58c9a2dd28b85b2dd1be93
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2024-10-16 08:17:20 +0000

    Deploying to gh-pages from @ ArtlessAvian/lizard-state-riir@0bc04c518896fbb365a5ddf9d634ed143cc6bf1c 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1729066120|4513283';
+const CACHE_VERSION = '1729066607|4752928';
 /** @type {string} */
 const CACHE_PREFIX = 'Lizard State 2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
